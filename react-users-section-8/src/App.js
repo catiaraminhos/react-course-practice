9e9026c9c2206e4c30e272441acfd4b290617f97
craminhos <craminhos@itclinical.com>
2021-07-22 18:27:48 +0100

    Display users list with dummy data

--- react-users-section-8/src/App.js
+++ react-users-section-8/src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import AddUser from './components/Users/AddUser/AddUser';
+import UsersList from './components/Users/UsersList/UsersList';
 
 const App = () => {
-  return (
-      <AddUser />
-  );
-}
+    const dummyUsers = [
+        {
+            id: 1,
+            name: 'Cátia',
+            age: 28,
+        },
+        {
+            id: 2,
+            name: 'Miguel',
+            age: 35
+        },
+    ];
+
+    return <div>
+        <AddUser />
+        <UsersList users={dummyUsers} />
+    </div>;
+};
 
 export default App;
