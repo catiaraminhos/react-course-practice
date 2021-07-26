bdfb45c895e369bae92974c6b76f2f35bc59a02d
craminhos <craminhos@itclinical.com>
2021-07-26 18:45:35 +0100

    Use portals in ErrorModal to move the HTML code closer to the HTML body

--- react-users-section-8/src/components/UI/ErrorModal.js
+++ react-users-section-8/src/components/UI/ErrorModal.js
@@ -1,28 +1,49 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import ReadDOM from 'react-dom';
 
 import Button from './Button';
 import Card from './Card';
 
 import styles from './ErrorModal.module.css';
 
+const Backdrop = (props) => {
+    return <div className={styles.backdrop} onClick={props.onConfirm} />;
+};
+
+const ModalOverlay = (props) => {
+    return (
+        <Card className={styles.modal}>
+            <header className={styles.header}>
+                <h2>{props.title}</h2>
+            </header>
+            <div className={styles.content}>
+                <p>{props.content}</p>
+            </div>
+            <footer className={styles.actions}>
+                <Button type="button" onClick={props.onConfirm}>
+                    Okay
+                </Button>
+            </footer>
+        </Card>
+    );
+};
+
 const ErrorModal = (props) => {
     return (
-        <div>
-            <div className={styles.backdrop} onClick={props.onConfirm} />
-            <Card className={styles.modal}>
-                <header className={styles.header}>
-                    <h2>{props.title}</h2>
-                </header>
-                <div className={styles.content}>
-                    <p>{props.content}</p>
-                </div>
-                <footer className={styles.actions}>
-                    <Button type="button" onClick={props.onConfirm}>
-                        Okay
-                    </Button>
-                </footer>
-            </Card>
-        </div>
+        <Fragment>
+            {ReadDOM.createPortal(
+                <Backdrop onConfirm={props.onConfirm} />,
+                document.getElementById('backdrop-root')
+            )}
+            {ReadDOM.createPortal(
+                <ModalOverlay
+                    title={props.title}
+                    content={props.content}
+                    onConfirm={props.onConfirm}
+                />,
+                document.getElementById('overlay-root')
+            )}
+        </Fragment>
     );
 };
 
