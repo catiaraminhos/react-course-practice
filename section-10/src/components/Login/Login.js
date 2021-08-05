ce7b57538dbc753702cc1a505666f8c640564a86
craminhos <craminhos@itclinical.com>
2021-08-05 22:11:24 +0100

    Use forwardRef to focus input when the login form is invalid

--- section-10/src/components/Login/Login.js
+++ section-10/src/components/Login/Login.js
@@ -1,4 +1,10 @@
-import React, { useState, useEffect, useReducer, useContext } from 'react';
+import React, {
+    useState,
+    useEffect,
+    useReducer,
+    useContext,
+    useRef,
+} from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
@@ -45,6 +51,9 @@ const Login = () => {
         isValid: null,
     });
 
+    const emailInputRef = useRef();
+    const passwordInputRef = useRef();
+
     const { isValid: emailIsValid } = emailState;
     const { isValid: passwordIsValid } = passwordState;
     useEffect(() => {
@@ -75,13 +84,21 @@ const Login = () => {
 
     const submitHandler = (event) => {
         event.preventDefault();
-        authCtx.onLogin(emailState.value, passwordState.value);
+
+        if (formIsValid) {
+            authCtx.onLogin(emailState.value, passwordState.value);
+        } else if (!emailIsValid) {
+            emailInputRef.current.focus();
+        } else {
+            passwordInputRef.current.focus();
+        }
     };
 
     return (
         <Card className={classes.login}>
             <form onSubmit={submitHandler}>
                 <Input
+                    ref={emailInputRef}
                     type="email"
                     id="email"
                     label="E-mail"
@@ -92,6 +109,7 @@ const Login = () => {
                 />
 
                 <Input
+                    ref={passwordInputRef}
                     type="password"
                     id="password"
                     label="Password"
@@ -102,11 +120,7 @@ const Login = () => {
                 />
 
                 <div className={classes.actions}>
-                    <Button
-                        type="submit"
-                        className={classes.btn}
-                        disabled={!formIsValid}
-                    >
+                    <Button type="submit" className={classes.btn}>
                         Login
                     </Button>
                 </div>
