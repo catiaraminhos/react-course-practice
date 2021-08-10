ca46db7e9ef5a0234b96e7fde2e4ce98d6ec0a1f
craminhos <craminhos@itclinical.com>
2021-08-10 18:42:24 +0100

    Init project and add Header component

---
+++ food-order-section-11/src/components/Layout/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import styles from './Header.module.css';
+import mealsImage from '../../assets/meals.jpg';
+
+const Header = () => {
+    return (
+        <React.Fragment>
+            <header className={styles.header}>
+                <h1>ReactMeals</h1>
+            </header>
+            <div className={styles['main-image']}>
+                <img src={mealsImage} alt="A table full of delicious food!" />
+            </div>
+        </React.Fragment>
+    );
+};
+
+export default Header;
