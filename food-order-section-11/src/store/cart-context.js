9b9c41705b53fb408401faf7fd4e429d3237bdab
Cátia Raminhos <catia.raminhos@daimler.com>
2022-03-07 19:19:15 +0000

    Fix typo in cart reducer

--- food-order-section-11/src/store/cart-context.js
+++ food-order-section-11/src/store/cart-context.js
@@ -16,7 +16,7 @@ const cartReducer = (state, action) => {
   if (action.type === 'ADD') {
     const updatedItems = state.items.concat(action.item);
     const updatedTotalAmount =
-      state.totalAmount + state.item.price * action.item.amount;
+      state.totalAmount + action.item.price * action.item.amount;
 
     return { items: updatedItems, totalAmount: updatedTotalAmount };
   }
