24896617aa39a7dfe3080382bb6ed523c7fa4ac9
Cátia Raminhos <catia.raminhos@daimler.com>
2022-03-07 18:42:14 +0000

    Manage cart state with cart reducer

--- food-order-section-11/src/store/cart-context.js
+++ food-order-section-11/src/store/cart-context.js
@@ -1,29 +1,61 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 
-const CartContext = React.createContext({
+export const CartContext = React.createContext({
   items: [],
   totalAmount: 0,
   addItem: (item) => {},
   removeItem: (id) => {}
 });
 
+const defaultCartState = {
+  items: [],
+  totalAmount: 0
+};
+
+const cartReducer = (state, action) => {
+  if (action.type === 'ADD') {
+    const updatedItems = state.items.concat(action.item);
+    const updatedTotalAmount =
+      state.totalAmount + state.item.price * action.item.amount;
+
+    return { items: updatedItems, totalAmount: updatedTotalAmount };
+  }
+
+  if (action.type === 'REMOVE') {
+    const itemToRemove = state.items.find((item) => item.id === action.id);
+    const updatedItems = state.items.filter((item) => item.id !== action.id);
+    const updatedTotalAmount =
+      state.totalAmount - itemToRemove.price * itemToRemove.amount;
+
+    return { items: updatedItems, totalAmount: updatedTotalAmount };
+  }
+
+  return defaultCartState;
+};
+
 export const CartContextProvider = (props) => {
-  const [items, setItems] = useState([]);
-  const [totalAmount, setTotalAmount] = useState(0);
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
 
-  const addItemHandler = (item) => {};
+  const addItemHandler = (item) => {
+    dispatchCartAction({ type: 'ADD', item: item });
+  };
 
-  const removeItemHandler = (id) => {};
+  const removeItemHandler = (id) => {
+    dispatchCartAction({ type: 'REMOVE', id: id });
+  };
+
+  const cartContext = {
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
+    addItem: addItemHandler,
+    removeItem: removeItemHandler
+  };
 
   return (
-    <CartContext.Provider
-      value={{
-        items: items,
-        totalAmount: totalAmount,
-        addItem: addItemHandler,
-        removeItem: removeItemHandler
-      }}
-    >
+    <CartContext.Provider value={cartContext}>
       {props.children}
     </CartContext.Provider>
   );
