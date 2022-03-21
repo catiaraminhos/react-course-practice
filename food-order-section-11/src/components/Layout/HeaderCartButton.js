0ef5fb03517a258eafdec4863ab39485fe186814
Cátia Raminhos <catia.raminhos@daimler.com>
2022-03-21 21:54:05 +0000

    Add animation to cart button when the cart items change

--- food-order-section-11/src/components/Layout/HeaderCartButton.js
+++ food-order-section-11/src/components/Layout/HeaderCartButton.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 
 import CartContext from '../../store/cart-context';
 import CartIcon from '../Cart/CartIcon';
@@ -6,13 +6,35 @@ import CartIcon from '../Cart/CartIcon';
 import styles from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnISHighlighted] = useState(false);
+
   const cartContext = useContext(CartContext);
-  const numberOfCartItems = cartContext.items.reduce((currentNumber, item) => {
+  const { items } = cartContext;
+
+  const numberOfCartItems = items.reduce((currentNumber, item) => {
     return currentNumber + item.amount;
   }, 0);
 
+  const btnClasses = `${styles.button} ${btnIsHighlighted ? styles.bump : ''}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+
+    setBtnISHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnISHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={styles.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
