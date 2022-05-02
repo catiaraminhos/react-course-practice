17bf8ab61cb4c2744a0d1b781a6a60a08007d39a
Cátia Raminhos <catia.raminhos@daimler.com>
2022-05-02 18:44:52 +0100

    Make useCounter also usable for BackwardCounter

--- simple-custom-hook-section-15/src/hooks/use-counter.js
+++ simple-custom-hook-section-15/src/hooks/use-counter.js
@@ -1,15 +1,15 @@
 import { useState, useEffect } from 'react';
 
-const useCounter = () => {
+const useCounter = (forwards = true) => {
   const [counter, setCounter] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCounter((prevCounter) => prevCounter + 1);
+      setCounter((prevCounter) => prevCounter + (forwards ? 1 : -1));
     }, 1000);
 
     return () => clearInterval(interval);
-  }, []);
+  }, [forwards]);
 
   return counter;
 };
