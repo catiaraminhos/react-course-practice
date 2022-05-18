012ca27c68a6b4973e37c356bcbfc46062b07de3
Cátia Raminhos <catia.raminhos@daimler.com>
2022-05-18 18:20:17 +0100

    Use useHttp custom hook in NewTask component

--- realistic-custom-hook-section-15/src/components/NewTask/NewTask.js
+++ realistic-custom-hook-section-15/src/components/NewTask/NewTask.js
@@ -1,41 +1,28 @@
-import { useState } from 'react';
-
+import useHttp from '../../hooks/use-http';
 import Section from '../UI/Section';
 import TaskForm from './TaskForm';
 
 const NewTask = (props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const { isLoading, error, sendRequest: addTask } = useHttp();
 
   const enterTaskHandler = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-course-http-4ee76-default-rtdb.europe-west1.firebasedatabase.app/tasks.json',
-        {
-          method: 'POST',
-          body: JSON.stringify({ text: taskText }),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
+    const afterAddTaskHandler = (data) => {
       const generatedId = data.name; // firebase-specific => "name" contains generated id
       const createdTask = { id: generatedId, text: taskText };
-
       props.onAddTask(createdTask);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
+    };
+
+    addTask(
+      {
+        url: 'https://react-course-http-4ee76-default-rtdb.europe-west1.firebasedatabase.app/tasks.json',
+        method: 'POST',
+        body: { text: taskText },
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      },
+      afterAddTaskHandler
+    );
   };
 
   return (
