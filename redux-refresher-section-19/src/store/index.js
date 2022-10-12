325248fa10b2c1cd0df754c0ca32939d8f3c39de
Cátia Raminhos <catia.raminhos@daimler.com>
2022-10-12 09:13:29 +0100

    Configure cart state and reducers

---
+++ redux-refresher-section-19/src/store/index.js
@@ -0,0 +1,54 @@
+import { configureStore, createSlice } from '@reduxjs/toolkit';
+
+const initialCartState = {
+  items: [],
+  showCart: false
+};
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState: initialCartState,
+  reducers: {
+    addItem: (state, action) => {
+      const itemToIncreaseQuantity = state.items.find(
+        (item) => item.title === action.payload.item.title
+      );
+
+      if (itemToIncreaseQuantity) {
+        itemToIncreaseQuantity.quantity++;
+        itemToIncreaseQuantity.total = itemToIncreaseQuantity.quantity * itemToIncreaseQuantity.price;
+      } else {
+        state.items.push({
+          title: action.payload.item.title,
+          quantity: 1,
+          price: action.payload.item.price,
+          total: action.payload.item.price
+        });
+      }
+    },
+    removeItem: (state, action) => {
+      const itemToDecreaseQuantity = state.items.find(
+        (item) => item.title === action.payload.itemTitle
+      );
+
+      if (itemToDecreaseQuantity.quantity === 1) {
+        const itemIndex = state.items.indexOf(itemToDecreaseQuantity);
+        state.items.splice(itemIndex, 1);
+      } else {
+        itemToDecreaseQuantity.quantity--;
+        itemToDecreaseQuantity.total = itemToDecreaseQuantity.quantity * itemToDecreaseQuantity.price;
+      }
+    },
+    toggleCart: (state) => {
+      state.showCart = !state.showCart;
+    }
+  }
+});
+
+const store = configureStore({
+  reducer: cartSlice.reducer
+});
+
+export const cartActions = cartSlice.actions;
+
+export default store;
