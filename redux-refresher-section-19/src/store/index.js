ada437b6788867adbae4d35eac4537ff98ac2fa2
Cátia Raminhos <catia.raminhos@daimler.com>
2022-10-12 09:17:15 +0100

    Refactor cart state and reducers

--- redux-refresher-section-19/src/store/index.js
+++ redux-refresher-section-19/src/store/index.js
@@ -1,54 +1,9 @@
-import { configureStore, createSlice } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 
-const initialCartState = {
-  items: [],
-  showCart: false
-};
-
-const cartSlice = createSlice({
-  name: 'cart',
-  initialState: initialCartState,
-  reducers: {
-    addItem: (state, action) => {
-      const itemToIncreaseQuantity = state.items.find(
-        (item) => item.title === action.payload.item.title
-      );
-
-      if (itemToIncreaseQuantity) {
-        itemToIncreaseQuantity.quantity++;
-        itemToIncreaseQuantity.total = itemToIncreaseQuantity.quantity * itemToIncreaseQuantity.price;
-      } else {
-        state.items.push({
-          title: action.payload.item.title,
-          quantity: 1,
-          price: action.payload.item.price,
-          total: action.payload.item.price
-        });
-      }
-    },
-    removeItem: (state, action) => {
-      const itemToDecreaseQuantity = state.items.find(
-        (item) => item.title === action.payload.itemTitle
-      );
-
-      if (itemToDecreaseQuantity.quantity === 1) {
-        const itemIndex = state.items.indexOf(itemToDecreaseQuantity);
-        state.items.splice(itemIndex, 1);
-      } else {
-        itemToDecreaseQuantity.quantity--;
-        itemToDecreaseQuantity.total = itemToDecreaseQuantity.quantity * itemToDecreaseQuantity.price;
-      }
-    },
-    toggleCart: (state) => {
-      state.showCart = !state.showCart;
-    }
-  }
-});
+import cartSliceReducer from './cart';
 
 const store = configureStore({
-  reducer: cartSlice.reducer
+  reducer: cartSliceReducer
 });
-
-export const cartActions = cartSlice.actions;
 
 export default store;
