1c243942c4a92ec20318bccf5ab610bf5a33b80a
Cátia Raminhos <catia.raminhos@daimler.com>
2022-10-12 09:35:29 +0100

    Toggle show cart

--- redux-refresher-section-19/src/components/Cart/CartButton.js
+++ redux-refresher-section-19/src/components/Cart/CartButton.js
@@ -1,12 +1,19 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { cartActions } from '../../store/cart';
 
 import classes from './CartButton.module.css';
 
 const CartButton = (props) => {
+  const dispatch = useDispatch();
   const nrItems = useSelector((state) => state.items.length);
 
+  const toggleCartHandler = () => {
+    dispatch(cartActions.toggleCart());
+  };
+
   return (
-    <button className={classes.button}>
+    <button onClick={toggleCartHandler} className={classes.button}>
       <span>My Cart</span>
       <span className={classes.badge}>{nrItems}</span>
     </button>
