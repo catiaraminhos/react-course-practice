2379f2e768bda7e777a1581b6d6b65cc397d144f
Cátia Raminhos <catia.raminhos@daimler.com>
2022-10-12 09:29:03 +0100

    Display number of cart items in header

--- redux-refresher-section-19/src/components/Cart/CartButton.js
+++ redux-refresher-section-19/src/components/Cart/CartButton.js
@@ -1,10 +1,14 @@
+import { useSelector } from 'react-redux';
+
 import classes from './CartButton.module.css';
 
 const CartButton = (props) => {
+  const nrItems = useSelector((state) => state.items.length);
+
   return (
     <button className={classes.button}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{nrItems}</span>
     </button>
   );
 };
