9212c0e560c85a64aebe257c8ce042887e6c79a3
Cátia Raminhos <catia.raminhos@daimler.com>
2022-10-31 09:54:10 +0000

    Change cart item key to use item id

--- redux-refresher-section-19/src/components/Cart/Cart.js
+++ redux-refresher-section-19/src/components/Cart/Cart.js
@@ -12,7 +12,7 @@ const Cart = (props) => {
       <h2>Your Shopping Cart</h2>
       <ul>
         {items.map((item) => (
-          <CartItem key={item.title} item={item} />
+          <CartItem key={item.id} item={item} />
         ))}
       </ul>
     </Card>
