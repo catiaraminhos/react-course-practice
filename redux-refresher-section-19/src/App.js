2f99320944136c0ae1d5274ae378828dfff66f74
Cátia Raminhos <catia.raminhos@daimler.com>
2022-11-09 10:38:03 +0000

    Move sendCartData to action creator

--- redux-refresher-section-19/src/App.js
+++ redux-refresher-section-19/src/App.js
@@ -5,7 +5,7 @@ import Notification from './components/UI/Notification';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-import { uiActions } from './store/ui';
+import { sendCartData } from './store/cart';
 
 let isInitial = true;
 
@@ -16,50 +16,12 @@ function App() {
   const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.showNotification({
-          status: 'pending',
-          title: 'Sending...',
-          message: 'Sending cart data!'
-        })
-      );
-
-      const response = await fetch(
-        'https://react-course-http-4ee76-default-rtdb.europe-west1.firebasedatabase.app/cart.json',
-        {
-          method: 'PUT',
-          body: JSON.stringify(cart)
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error('Sending cart data failed');
-      }
-
-      dispatch(
-        uiActions.showNotification({
-          status: 'success',
-          title: 'Success!',
-          message: 'Sent cart data successfully!'
-        })
-      );
-    };
-
     if (isInitial) {
       isInitial = false;
       return;
     }
 
-    sendCartData().catch(() => {
-      dispatch(
-        uiActions.showNotification({
-          status: 'error',
-          title: 'Error!',
-          message: 'Sending cart data failed!'
-        })
-      );
-    });
+    dispatch(sendCartData(cart));
   }, [cart, dispatch]);
 
   return (
