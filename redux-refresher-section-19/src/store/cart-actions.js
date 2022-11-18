c7cd8cf627a4c288a9c5b130895e60d2e0a4f3cd
Cátia Raminhos <catia.raminhos@daimler.com>
2022-11-18 09:13:07 +0000

    Only send items and total quantity to Firebase

--- redux-refresher-section-19/src/store/cart-actions.js
+++ redux-refresher-section-19/src/store/cart-actions.js
@@ -47,7 +47,10 @@ export const sendCartData = (cart) => {
         'https://react-course-http-4ee76-default-rtdb.europe-west1.firebasedatabase.app/cart.json',
         {
           method: 'PUT',
-          body: JSON.stringify(cart)
+          body: JSON.stringify({
+            items: cart.items,
+            totalQuantity: cart.totalQuantity
+          })
         }
       );
   
