15b97a944efd6a693ed5e14ef6a14cbd94d1d0b0
Cátia Raminhos <catia.raminhos@daimler.com>
2022-12-09 09:29:04 +0000

    Setup simple version of All Quotes page

--- react-router-practice-section-18/src/App.js
+++ react-router-practice-section-18/src/App.js
@@ -1,7 +1,19 @@
+import { Route } from 'react-router-dom';
+import Layout from './components/layout/Layout';
+import MainNavigation from './components/layout/MainNavigation';
+
+import AllQuotes from './pages/AllQuotes';
+
 function App() {
   return (
     <div>
+      <MainNavigation />
 
+      <Layout>
+        <Route path="/quotes">
+          <AllQuotes />
+        </Route>
+      </Layout>
     </div>
   );
 }
