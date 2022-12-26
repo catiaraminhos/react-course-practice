1a5796555ea149176a155f97a65765bdaf96ede1
Cátia Raminhos <catia.raminhos@daimler.com>
2022-12-26 21:14:55 +0000

    Display Comments section when in comments url

--- react-router-practice-section-20/src/pages/QuoteDetail.js
+++ react-router-practice-section-20/src/pages/QuoteDetail.js
@@ -1,12 +1,19 @@
-import { useParams } from 'react-router-dom';
+import { Route, useParams } from 'react-router-dom';
+import Comments from '../components/comments/Comments';
 
 const QuoteDetail = () => {
   const params = useParams();
 
-  return <section>
-    <h1>Quote Details</h1>
-    <p>{params.quoteId}</p>
-  </section>;
+  return (
+    <section>
+      <h1>Quote Details</h1>
+      <p>{params.quoteId}</p>
+
+      <Route path={`/quotes/${params.quoteId}/comments`}>
+        <Comments />
+      </Route>
+    </section>
+  );
 };
 
 export default QuoteDetail;
