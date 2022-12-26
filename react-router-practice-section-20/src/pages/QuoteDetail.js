cd13074b206bfe8e1b500a1dff320d9453716df7
Cátia Raminhos <catia.raminhos@daimler.com>
2022-12-26 21:48:20 +0000

    Display quote information on Quote Detail page

--- react-router-practice-section-20/src/pages/QuoteDetail.js
+++ react-router-practice-section-20/src/pages/QuoteDetail.js
@@ -1,13 +1,25 @@
 import { Route, useParams } from 'react-router-dom';
+
+import HighlightedQuote from '../components/quotes/HighlightedQuote';
 import Comments from '../components/comments/Comments';
+
+const DUMMY_QUOTES = [
+  { id: 'q1', author: 'Max', text: 'Learning React is fun!' },
+  { id: 'q2', author: 'Maximilian', text: 'Learning React is great!' }
+];
 
 const QuoteDetail = () => {
   const params = useParams();
 
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No quote found!</p>
+  }
+
   return (
     <section>
-      <h1>Quote Details</h1>
-      <p>{params.quoteId}</p>
+      <HighlightedQuote author={quote.author} text={quote.text} />
 
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
