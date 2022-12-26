20e13d12f45a28497c99264623c40a2119633f0b
Cátia Raminhos <catia.raminhos@daimler.com>
2022-12-26 21:20:47 +0000

    Refactor Layout component

--- react-router-practice-section-20/src/App.js
+++ react-router-practice-section-20/src/App.js
@@ -1,7 +1,6 @@
 import { Redirect, Route, Switch } from 'react-router-dom';
 
 import Layout from './components/layout/Layout';
-import MainNavigation from './components/layout/MainNavigation';
 
 import AllQuotes from './pages/AllQuotes';
 import AddQuote from './pages/AddQuote';
@@ -9,26 +8,22 @@ import QuoteDetail from './pages/QuoteDetail';
 
 function App() {
   return (
-    <div>
-      <MainNavigation />
-
-      <Layout>
-        <Switch>
-          <Route exact path="/">
-            <Redirect to="/quotes" />
-          </Route>
-          <Route exact path="/quotes">
-            <AllQuotes />
-          </Route>
-          <Route path="/quotes/:quoteId">
-            <QuoteDetail />
-          </Route>
-          <Route path="/add-quote">
-            <AddQuote />
-          </Route>
-        </Switch>
-      </Layout>
-    </div>
+    <Layout>
+      <Switch>
+        <Route exact path="/">
+          <Redirect to="/quotes" />
+        </Route>
+        <Route exact path="/quotes">
+          <AllQuotes />
+        </Route>
+        <Route path="/quotes/:quoteId">
+          <QuoteDetail />
+        </Route>
+        <Route path="/add-quote">
+          <AddQuote />
+        </Route>
+      </Switch>
+    </Layout>
   );
 }
 
--- react-router-practice-section-20/src/components/layout/Layout.js
+++ react-router-practice-section-20/src/components/layout/Layout.js
@@ -1,7 +1,15 @@
+import { Fragment } from 'react';
+
 import styles from './Layout.module.css';
+import MainNavigation from './MainNavigation';
 
 const Layout = (props) => {
-  return <div className={styles.main}>{props.children}</div>;
+  return (
+    <Fragment>
+      <MainNavigation />
+      <main className={styles.main}>{props.children}</main>
+    </Fragment>
+  );
 };
 
 export default Layout;
