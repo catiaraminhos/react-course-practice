b61a43785ec9a38d656aa58ad0b8d7dab3f1c3ae
Cátia Raminhos <catia.raminhos@daimler.com>
2022-12-26 23:22:49 +0000

    Navigate back to quotes after adding a new quote

--- react-router-practice-section-20/src/pages/AddQuote.js
+++ react-router-practice-section-20/src/pages/AddQuote.js
@@ -1,8 +1,13 @@
+import { useHistory } from 'react-router-dom';
+
 import QuoteForm from '../components/quotes/QuoteForm';
 
 const AddQuote = () => {
+  const navigationHistory = useHistory();
+
   const addQuoteHandler = (quoteData) => {
     console.log(quoteData);
+    navigationHistory.push('/quotes');
   };
 
   return <QuoteForm onAddQuote={addQuoteHandler} />;
