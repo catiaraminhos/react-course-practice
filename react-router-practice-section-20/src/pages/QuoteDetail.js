f10d2d6a6522f2094f29db7ca82365eb5dd55573
Cátia Raminhos <catia.raminhos@daimler.com>
2022-12-29 15:55:35 +0000

    Refactor Quote Detail page

--- react-router-practice-section-20/src/pages/QuoteDetail.js
+++ react-router-practice-section-20/src/pages/QuoteDetail.js
@@ -1,4 +1,4 @@
-import { Link, Route, useParams } from 'react-router-dom';
+import { Link, Route, useParams, useRouteMatch } from 'react-router-dom';
 
 import HighlightedQuote from '../components/quotes/HighlightedQuote';
 import Comments from '../components/comments/Comments';
@@ -9,6 +9,7 @@ const DUMMY_QUOTES = [
 ];
 
 const QuoteDetail = () => {
+  const match = useRouteMatch();
   const params = useParams();
 
   const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
@@ -21,15 +22,15 @@ const QuoteDetail = () => {
     <section>
       <HighlightedQuote author={quote.author} text={quote.text} />
 
-      <Route exact path={`/quotes/${params.quoteId}`}>
+      <Route exact path={match.path}>
         <div className="centered">
-          <Link className="btn--flat" to={`/quotes/${params.quoteId}/comments`}>
+          <Link className="btn--flat" to={`${match.url}/comments`}>
             Load Comments
           </Link>
         </div>
       </Route>
 
-      <Route path={`/quotes/${params.quoteId}/comments`}>
+      <Route path={`${match.path}/comments`}>
         <Comments />
       </Route>
     </section>
