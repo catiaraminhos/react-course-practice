9818d41b742cbc4f8fa02d006fba22da6b6bfb21
Cátia Raminhos <catia.raminhos@daimler.com>
2023-01-05 22:13:59 +0000

    Simplify data fetch for Post Detail page

--- react-router-v6-data-fetch-section-20/src/App.jsx
+++ react-router-v6-data-fetch-section-20/src/App.jsx
@@ -8,7 +8,7 @@ import {
 import BlogLayout from './pages/BlogLayout';
 import BlogPostsPage, { loader as blogPostsLoader } from './pages/BlogPosts';
 import NewPostPage from './pages/NewPost';
-import PostDetailPage from './pages/PostDetail';
+import PostDetailPage, { loader as postDetailLoader } from './pages/PostDetail';
 import RootLayout from './components/RootLayout';
 import WelcomePage from './pages/Welcome';
 
@@ -18,7 +18,7 @@ const router = createBrowserRouter(
       <Route index element={<WelcomePage />} />
       <Route path="/blog" element={<BlogLayout />}>
         <Route index element={<BlogPostsPage />} loader={blogPostsLoader} />
-        <Route path=":id" element={<PostDetailPage />} />
+        <Route path=":id" element={<PostDetailPage />} loader={postDetailLoader} />
       </Route>
       <Route path="/blog/new" element={<NewPostPage />} />
     </Route>
