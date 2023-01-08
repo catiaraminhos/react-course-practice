10f5fdfb2e1ae6ca4c1778c2768f3f71b2c453f9
Cátia Raminhos <catia.raminhos@daimler.com>
2023-01-08 18:01:08 +0000

    Add newsletter signup on Post Detail page

--- react-router-v6-data-fetch-section-20/src/pages/PostDetail.jsx
+++ react-router-v6-data-fetch-section-20/src/pages/PostDetail.jsx
@@ -1,21 +1,24 @@
 import { useLoaderData } from 'react-router-dom';
 
 import BlogPost from '../components/BlogPost';
+import NewsletterSignup from '../components/NewsletterSignup';
 import { getPost } from '../util/api';
 
 function PostDetailPage() {
-  const loadedPost = useLoaderData();
-
-  console.log(loadedPost);
+  const postData = useLoaderData();
 
   return (
-      <BlogPost title={loadedPost.title} text={loadedPost.body} />
+    <>
+      <BlogPost title={postData.title} text={postData.body} />
+      <NewsletterSignup />
+    </>
   );
 }
 
 export default PostDetailPage;
 
-export const loader = ({params}) => {
+export function loader({ params }) {
   const postId = params.id;
+
   return getPost(postId);
-};
+}
