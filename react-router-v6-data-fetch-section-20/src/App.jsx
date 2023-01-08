92e2808398fafa6026c4581c4112ce9815cf7b07
Cátia Raminhos <catia.raminhos@daimler.com>
2023-01-08 17:32:57 +0000

    Defer loading of Blog Posts

--- react-router-v6-data-fetch-section-20/src/App.jsx
+++ react-router-v6-data-fetch-section-20/src/App.jsx
@@ -12,20 +12,31 @@ import PostDetailPage, { loader as postDetailLoader } from './pages/PostDetail';
 import RootLayout from './components/RootLayout';
 import WelcomePage from './pages/Welcome';
 import ErrorPage from './pages/Error';
+import DeferredBlogPostsPage, {
+  loader as deferredBlogPostsLoader
+} from './pages/DeferredBlogPosts';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />} errorElement={<ErrorPage />}>
       <Route index element={<WelcomePage />} />
       <Route path="/blog" element={<BlogLayout />}>
-        <Route index element={<BlogPostsPage />} loader={blogPostsLoader} />
+        <Route
+          index
+          element={<DeferredBlogPostsPage />}
+          loader={deferredBlogPostsLoader}
+        />
         <Route
           path=":id"
           element={<PostDetailPage />}
           loader={postDetailLoader}
         />
       </Route>
-      <Route path="/blog/new" element={<NewPostPage />} action={newPostAction} />
+      <Route
+        path="/blog/new"
+        element={<NewPostPage />}
+        action={newPostAction}
+      />
     </Route>
   )
 );
--- react-router-v6-data-fetch-section-20/src/util/api.js
+++ react-router-v6-data-fetch-section-20/src/util/api.js
@@ -1,7 +1,18 @@
+import { sleep } from './sleep';
+
 export async function getPosts() {
   const response = await fetch('https://jsonplaceholder.typicode.com/posts');
   if (!response.ok) {
     throw { message: 'Failed to fetch posts.', status: 500 };
+  }
+  return response.json();
+}
+
+export async function getSlowPosts() {
+  await sleep(2000);
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  if (!response.ok) {
+    throw new Response('Failed to fetch posts.', { status: 500 });
   }
   return response.json();
 }
@@ -25,8 +36,8 @@ export async function savePost(post) {
     method: 'POST',
     body: JSON.stringify(post),
     headers: {
-      'Content-Type': 'application/json',
-    },
+      'Content-Type': 'application/json'
+    }
   });
 
   if (!response.ok) {
