b703db4f8c83421d8072f8f1749c135d8b2837eb
Cátia Raminhos <catia.raminhos@daimler.com>
2023-01-08 16:18:22 +0000

    Simplify post submission on New Post page

--- react-router-v6-data-fetch-section-20/src/pages/NewPost.jsx
+++ react-router-v6-data-fetch-section-20/src/pages/NewPost.jsx
@@ -1,30 +1,12 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { redirect, useActionData, useNavigate, useNavigation } from 'react-router-dom';
 
 import NewPostForm from '../components/NewPostForm';
 import { savePost } from '../util/api';
 
 function NewPostPage() {
-  const [isSubmitting, setIsSubmitting] = useState(false);
-  const [error, setError] = useState();
+  const actionData = useActionData();
   const navigate = useNavigate();
-
-  async function submitHandler(event) {
-    event.preventDefault();
-    setIsSubmitting(true);
-    try {
-      const formData = new FormData(event.target);
-      const post = {
-        title: formData.get('title'),
-        body: formData.get('post-text'),
-      };
-      await savePost(post);
-      navigate('/');
-    } catch (err) {
-      setError(err);
-    }
-    setIsSubmitting(false);
-  }
+  const navigation = useNavigation();
 
   function cancelHandler() {
     navigate('/blog');
@@ -32,14 +14,30 @@ function NewPostPage() {
 
   return (
     <>
-      {error && <p>{error.message}</p>}
-      <NewPostForm
-        onCancel={cancelHandler}
-        onSubmit={submitHandler}
-        submitting={isSubmitting}
-      />
+      {actionData && actionData.status && <p>{actionData.message}</p>}
+      <NewPostForm onCancel={cancelHandler} submitting={navigation.state === 'submitting'} />
     </>
   );
 }
 
 export default NewPostPage;
+
+export async function action({ request }) {
+  const formData = await request.formData();
+  const post = {
+    title: formData.get('title'),
+    body: formData.get('post-text')
+  };
+
+  try {
+    await savePost(post);
+  } catch (error) {
+    if (error.status === 422) {
+      return error;
+    }
+
+    throw error;
+  }
+
+  return redirect('/blog');
+}
