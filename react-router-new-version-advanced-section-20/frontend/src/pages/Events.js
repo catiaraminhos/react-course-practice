737c0c4e9cfd782a7d6d1a95ef784d0fdf30d9ff
Cátia Raminhos <catia.raminhos@daimler.com>
2023-02-06 18:59:48 +0000

    Deferring events fetching on All Events page

--- react-router-new-version-advanced-section-20/frontend/src/pages/Events.js
+++ react-router-new-version-advanced-section-20/frontend/src/pages/Events.js
@@ -1,23 +1,28 @@
-import { json, useLoaderData } from 'react-router-dom';
+import { Suspense } from 'react';
+import { Await, defer, json, useLoaderData } from 'react-router-dom';
 
 import EventsList from '../components/EventsList';
 
 function EventsPage() {
-  const data = useLoaderData();
+  const { events } = useLoaderData();
 
   // Other implementations options
   // if (data.isError) {
   //   return <p>{data.message}</p>;
   // }
 
-  const events = data.events;
-
-  return <EventsList events={events} />;
+  return (
+    <Suspense fallback={<p style={{ textAlign: 'center' }}>Loading...</p>}>
+      <Await resolve={events}>
+        {(loadedEvents) => <EventsList events={loadedEvents} />}
+      </Await>
+    </Suspense>
+  );
 }
 
 export default EventsPage;
 
-export const loader = async () => {
+const loadEvents = async () => {
   const response = await fetch('http://localhost:8081/events');
 
   if (!response.ok) {
@@ -33,6 +38,13 @@ export const loader = async () => {
 
     throw json({ message: 'Could not fetch events.' }, { status: 500 });
   } else {
-    return response;
+    const resData = await response.json();
+    return resData.events;
   }
+};
+
+export const loader = () => {
+  return defer({
+    events: loadEvents()
+  });
 };
