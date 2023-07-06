5e95be72b58621d83e9294a6c432ccfc7d586f64
Cátia Raminhos <catia.raminhos@daimler.com>
2023-07-06 11:23:20 +0100

    Do not fetch ingredients on every key press

--- react-hooks-section-29/src/components/Ingredients/Search.js
+++ react-hooks-section-29/src/components/Ingredients/Search.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
@@ -6,31 +6,36 @@ import './Search.css';
 const Search = React.memo((props) => {
   const { onLoadIngredients } = props;
   const [enteredFilter, setEnteredFilter] = useState('');
+  const filterInputRef = useRef();
 
   useEffect(() => {
-    const query =
-      enteredFilter.length === 0
-        ? ''
-        : `?orderBy="title"&equalTo="${enteredFilter}"`;
+    setTimeout(() => {
+      if (enteredFilter === filterInputRef.current.value) {
+        const query =
+          enteredFilter.length === 0
+            ? ''
+            : `?orderBy="title"&equalTo="${enteredFilter}"`;
 
-    fetch(
-      'https://react-hooks-update-e8a96-default-rtdb.europe-west1.firebasedatabase.app/ingredients.json' +
-        query
-    )
-      .then((response) => response.json())
-      .then((responseData) => {
-        const loadedIngredients = [];
+        fetch(
+          'https://react-hooks-update-e8a96-default-rtdb.europe-west1.firebasedatabase.app/ingredients.json' +
+            query
+        )
+          .then((response) => response.json())
+          .then((responseData) => {
+            const loadedIngredients = [];
 
-        for (const key in responseData) {
-          loadedIngredients.push({
-            id: key,
-            title: responseData[key].title,
-            amount: responseData[key].amount
+            for (const key in responseData) {
+              loadedIngredients.push({
+                id: key,
+                title: responseData[key].title,
+                amount: responseData[key].amount
+              });
+            }
+            onLoadIngredients(loadedIngredients);
           });
-        }
-        onLoadIngredients(loadedIngredients);
-      });
-  }, [enteredFilter, onLoadIngredients]);
+      }
+    }, 500);
+  }, [enteredFilter, onLoadIngredients, filterInputRef]);
 
   const filterChangeHandler = (event) => {
     setEnteredFilter(event.target.value);
@@ -45,6 +50,7 @@ const Search = React.memo((props) => {
             type="text"
             value={enteredFilter}
             onChange={filterChangeHandler}
+            ref={filterInputRef}
           />
         </div>
       </Card>
