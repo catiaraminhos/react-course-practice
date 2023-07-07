516ceed7b87c6c1958b4e397e66d231946aa497d
Cátia Raminhos <catia.raminhos@daimler.com>
2023-07-07 12:02:09 +0100

    Add dummy login

--- react-hooks-section-29/src/App.js
+++ react-hooks-section-29/src/App.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
+import { AuthContext } from './context/auth-context';
+import Auth from './components/Auth';
 import Ingredients from './components/Ingredients/Ingredients';
 
-const App = props => {
-  return <Ingredients />;
+const App = (props) => {
+  const authContext = useContext(AuthContext);
+
+  return authContext.isAuth ? <Ingredients /> : <Auth />;
 };
 
 export default App;
