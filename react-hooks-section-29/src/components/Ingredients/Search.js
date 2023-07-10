f7762b1d9ac467ef8e440b41fe460a4135703300
Cátia Raminhos <catia.raminhos@daimler.com>
2023-07-10 09:43:23 +0100

    Use useHttp hook also in Search component

--- react-hooks-section-29/src/components/Ingredients/Search.js
+++ react-hooks-section-29/src/components/Ingredients/Search.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect, useRef } from 'react';
 
 import Card from '../UI/Card';
+import ErrorModal from '../UI/ErrorModal';
+import useHttp from '../../hooks/http';
 import './Search.css';
 
 const Search = React.memo((props) => {
   const { onLoadIngredients } = props;
   const [enteredFilter, setEnteredFilter] = useState('');
   const filterInputRef = useRef();
+  const { isLoading, data, error, sendRequest, clearError } = useHttp();
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -16,23 +19,11 @@ const Search = React.memo((props) => {
             ? ''
             : `?orderBy="title"&equalTo="${enteredFilter}"`;
 
-        fetch(
+        const url =
           'https://react-hooks-update-e8a96-default-rtdb.europe-west1.firebasedatabase.app/ingredients.json' +
-            query
-        )
-          .then((response) => response.json())
-          .then((responseData) => {
-            const loadedIngredients = [];
+          query;
 
-            for (const key in responseData) {
-              loadedIngredients.push({
-                id: key,
-                title: responseData[key].title,
-                amount: responseData[key].amount
-              });
-            }
-            onLoadIngredients(loadedIngredients);
-          });
+        sendRequest(url, 'GET');
       }
     }, 500);
 
@@ -40,7 +31,23 @@ const Search = React.memo((props) => {
       // Runs before the next time useEffect will run
       clearTimeout(timer);
     };
-  }, [enteredFilter, onLoadIngredients, filterInputRef]);
+  }, [enteredFilter, filterInputRef, sendRequest]);
+
+  useEffect(() => {
+    if (!isLoading && !error && data) {
+      const loadedIngredients = [];
+
+      for (const key in data) {
+        loadedIngredients.push({
+          id: key,
+          title: data[key].title,
+          amount: data[key].amount
+        });
+      }
+
+      onLoadIngredients(loadedIngredients);
+    }
+  }, [data, isLoading, error, onLoadIngredients]);
 
   const filterChangeHandler = (event) => {
     setEnteredFilter(event.target.value);
@@ -48,9 +55,11 @@ const Search = React.memo((props) => {
 
   return (
     <section className="search">
+      {error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
+          {isLoading && <span>Loading...</span>}
           <input
             type="text"
             value={enteredFilter}
