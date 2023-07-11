c1912d1fac9c94fe95875e108bbd4345f6ebb9c9
Cátia Raminhos <catia.raminhos@daimler.com>
2023-07-11 09:58:57 +0100

    Add form to add new post

--- react-core-features-section-30/src/components/PostsList.jsx
+++ react-core-features-section-30/src/components/PostsList.jsx
@@ -1,13 +1,18 @@
+import NewPost from './NewPost';
 import Post from './Post';
 
 import styles from './PostsList.module.css';
 
 const PostsList = () => {
   return (
-    <ul className={styles.posts}>
-      <Post author="Maximilian" text="React.js is awesome!" />
-      <Post author="Manuel" text="Checkout the full course!" />
-    </ul>
+    <>
+      <NewPost />
+
+      <ul className={styles.posts}>
+        <Post author="Maximilian" text="React.js is awesome!" />
+        <Post author="Manuel" text="Checkout the full course!" />
+      </ul>
+    </>
   );
 };
 
