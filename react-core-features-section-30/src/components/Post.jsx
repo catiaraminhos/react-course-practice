34f585b044459bb91ba4288edc6f776a1368a4f9
Cátia Raminhos <catia.raminhos@daimler.com>
2023-07-11 16:03:28 +0100

    Simplify props on Post component

--- react-core-features-section-30/src/components/Post.jsx
+++ react-core-features-section-30/src/components/Post.jsx
@@ -1,10 +1,10 @@
 import styles from './Post.module.css';
 
-const Post = (props) => {
+const Post = ({ author, text }) => {
   return (
     <li className={styles.post}>
-      <p className={styles.author}>{props.author}</p>
-      <p className={styles.text}>{props.text}</p>
+      <p className={styles.author}>{author}</p>
+      <p className={styles.text}>{text}</p>
     </li>
   );
 };
