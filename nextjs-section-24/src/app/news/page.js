7fc26f536d64597fa45ad38568c2b1c2ea666c22
Cátia Raminhos <catia.raminhos@daimler.com>
2023-08-07 10:40:05 +0100

    Add list of news to News Page

--- nextjs-section-24/src/app/news/page.js
+++ nextjs-section-24/src/app/news/page.js
@@ -1,5 +1,19 @@
+import Link from 'next/link';
+
 const NewsPage = () => {
-  return <h1>The News Page</h1>;
+  return (
+    <>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/next-js-is-great">NextJS Is A Great Framework</Link>
+        </li>
+        <li>
+          <Link href="/news/something-else">Something Else</Link>
+        </li>
+      </ul>
+    </>
+  );
 };
 
 export default NewsPage;
