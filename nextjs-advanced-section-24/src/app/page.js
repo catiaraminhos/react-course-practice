83e38cb85d30af63b4637043f6b86f1d74d4a9f5
Cátia Raminhos <catia.raminhos@daimler.com>
2023-08-08 09:19:06 +0100

    Display dummy meetup list

--- nextjs-advanced-section-24/src/app/page.js
+++ nextjs-advanced-section-24/src/app/page.js
@@ -1,5 +1,25 @@
+import MeetupList from '@/components/meetups/MeetupList';
+
+const DUMMY_MEETUPS = [
+  {
+    id: 'm1',
+    title: 'A First Meetup',
+    image: 'https://blog.euvou.events/wp-content/uploads/2021/06/meetup.png',
+    address: 'Some address 5, 12345 Some City',
+    description: 'This is a first meetup!'
+  },
+  {
+    id: 'm2',
+    title: 'A Second Meetup',
+    image:
+      'https://cdn.britannica.com/73/114973-050-2DC46083/Midtown-Manhattan-Empire-State-Building-New-York.jpg',
+    address: 'Some address 10, 12345 Some City',
+    description: 'This is a second meetup!'
+  }
+];
+
 const HomePage = () => {
-  return <h1>Home Page</h1>;
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
 };
 
 export default HomePage;
