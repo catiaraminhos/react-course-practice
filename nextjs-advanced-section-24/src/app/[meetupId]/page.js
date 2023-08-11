0eabd23a2a20b5a7609a8c2d02c0422901a29ae4
Cátia Raminhos <catia.raminhos@daimler.com>
2023-08-11 09:44:13 +0100

    Simulate meetup details fetching on server side

--- nextjs-advanced-section-24/src/app/[meetupId]/page.js
+++ nextjs-advanced-section-24/src/app/[meetupId]/page.js
@@ -1,12 +1,20 @@
 import MeetupDetails from '@/components/meetups/MeetupDetails';
 
-const MeetupDetailsPage = () => {
-  const meetup = {
-    title: 'A First Meetup',
-    image: 'https://blog.euvou.events/wp-content/uploads/2021/06/meetup.png',
-    address: 'Some address 5, 12345 Some City',
-    description: 'This is a first meetup!'
-  };
+const MEETUP = {
+  title: 'A First Meetup',
+  image: 'https://blog.euvou.events/wp-content/uploads/2021/06/meetup.png',
+  address: 'Some address 5, 12345 Some City',
+  description: 'This is a first meetup!'
+};
+
+const getMeetup = async (id) => {
+  console.log('meetup id', id);
+
+  return { ...MEETUP, id };
+};
+
+const MeetupDetailsPage = async ({ params }) => {
+  const meetup = await getMeetup(params.meetupId);
 
   return <MeetupDetails {...meetup} />;
 };
