d480b2dce213bafab31f30aec53ae32a4a78b78f
Cátia Raminhos <catia.raminhos@daimler.com>
2023-08-22 17:23:29 +0100

    Send new meetup data to new API

--- nextjs-advanced-section-24/src/app/new-meetup/page.js
+++ nextjs-advanced-section-24/src/app/new-meetup/page.js
@@ -1,10 +1,24 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
+
 import NewMeetupForm from '@/components/meetups/NewMeetupForm';
 
 const NewMeetupPage = () => {
-  const addMeetupHandler = (meetup) => {
-    console.log('meetup to add', meetup);
+  const router = useRouter();
+
+  const addMeetupHandler = async (meetup) => {
+    const response = await fetch('/api/meetup', {
+      method: 'POST',
+      body: JSON.stringify(meetup),
+      headers: { 'Content-Type': 'application/json' }
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+
+    router.push('/');
   };
 
   return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
