653979fe0cca8dd5990998eed8ce9116dcc43c50
Cátia Raminhos <catia.raminhos@daimler.com>
2023-08-29 09:26:13 +0100

    Remove dummy data

--- nextjs-advanced-section-24/src/app/page.js
+++ nextjs-advanced-section-24/src/app/page.js
@@ -2,24 +2,6 @@ import { getMeetups } from './api/meetups/meetups';
 
 import MeetupList from '@/components/meetups/MeetupList';
 
-const DUMMY_MEETUPS = [
-  {
-    id: 'm1',
-    title: 'A First Meetup',
-    image: 'https://blog.euvou.events/wp-content/uploads/2021/06/meetup.png',
-    address: 'Some address 5, 12345 Some City',
-    description: 'This is a first meetup!'
-  },
-  {
-    id: 'm2',
-    title: 'A Second Meetup',
-    image:
-      'https://cdn.britannica.com/73/114973-050-2DC46083/Midtown-Manhattan-Empire-State-Building-New-York.jpg',
-    address: 'Some address 10, 12345 Some City',
-    description: 'This is a second meetup!'
-  }
-];
-
 const HomePage = async () => {
   const meetups = await getMeetups();
 
