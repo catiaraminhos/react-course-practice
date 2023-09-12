0948e372477ca26e58ee5594777ced155e9958b0
Cátia Raminhos <catia.raminhos@daimler.com>
2023-09-12 09:34:44 +0100

    Delete event in the details page

--- react-query-section/src/components/Events/EventDetails.jsx
+++ react-query-section/src/components/Events/EventDetails.jsx
@@ -1,17 +1,38 @@
-import { Link, Outlet, useParams } from 'react-router-dom';
-import { useQuery } from '@tanstack/react-query';
+import { Link, Outlet, useNavigate, useParams } from 'react-router-dom';
+import { useMutation, useQuery } from '@tanstack/react-query';
 
 import LoadingIndicator from '../UI/LoadingIndicator.jsx';
 import ErrorBlock from '../UI/ErrorBlock.jsx';
 import Header from '../Header.jsx';
-import { fetchEvent } from '../../util/http.js';
+import { deleteEvent, fetchEvent, queryClient } from '../../util/http.js';
 
 export default function EventDetails() {
+  const navigate = useNavigate();
   const { id } = useParams();
+
   const { data, isLoading, isError, error } = useQuery({
     queryKey: ['events', { id }],
     queryFn: ({ signal }) => fetchEvent({ id, signal })
   });
+
+  const {
+    mutate,
+    isPending: isPendingDeletion,
+    isError: isErrorOnDeletion,
+    error: errorOnDeletion
+  } = useMutation({
+    mutationFn: deleteEvent,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['events']
+      });
+      navigate('/events');
+    }
+  });
+
+  const handleDeleteEvent = () => {
+    mutate({ id });
+  };
 
   let detailsContent;
 
@@ -46,10 +67,22 @@ export default function EventDetails() {
         <header>
           <h1>{data.title}</h1>
           <nav>
-            <button>Delete</button>
+            <button onClick={handleDeleteEvent}>
+              {isPendingDeletion ? 'Deleting...' : 'Delete'}
+            </button>
             <Link to="edit">Edit</Link>
           </nav>
+
+          {isErrorOnDeletion && (
+            <ErrorBlock
+              title="An error occurred"
+              message={
+                errorOnDeletion.info?.message || 'Failed to delete event.'
+              }
+            />
+          )}
         </header>
+
         <div id="event-details-content">
           <img src={`http://localhost:3000/${data.image}`} alt={data.title} />
           <div id="event-details-info">
